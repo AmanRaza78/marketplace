d4c527680d7c54e22612e745cfdf1ea8db03545c
AmanRaza78 <razakhanaman9@gmail.com>
2024-06-20 23:07:25 +0530

    created the my products page

---
+++ src/app/my-products/page.tsx
@@ -0,0 +1,48 @@
+import ProductCard from "@/components/product-card";
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function getData(userId: string) {
+  const data = await prisma.product.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      name: true,
+      images: true,
+      price: true,
+      smalldescription: true,
+      id: true,
+    },
+  });
+  return data
+}
+
+export default async function MyProductsPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("Not Authorized");
+  }
+
+  const data = await getData(user.id);
+
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8">
+      <h1 className="text-2xl font-bold">My Products</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 sm:grid-cols-2 mt-4">
+        {data.map((item) => (
+          <ProductCard
+            key={item.id}
+            id={item.id}
+            images={item.images}
+            name={item.name}
+            price={item.price}
+            smalldescription={item.smalldescription}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
